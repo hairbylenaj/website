fefaa80e810cc1818747205a0f4564fb13fd3b2a
Andreas Beasley <andreas@sapioweb.com>
2019-01-16 21:13:15 -0800

    mayven removal

--- server.js
+++ server.js
@@ -1,39 +1,39 @@
-var express = require('express')
-var ejs = require('ejs')
-var engine = require('ejs-blocks')
-var bugsnag = require('bugsnag')
+var express = require('express');
+var ejs = require('ejs');
+var engine = require('ejs-blocks');
+var bugsnag = require('bugsnag');
 var bodyParser = require('body-parser');
-var dotenv = require('dotenv')
+var dotenv = require('dotenv');
 
-var app = express()
+var app = express();
 
 // Load .env
 dotenv.load();
 
 // Inject routes
-var routes = require('./app/routes/routes')
+var routes = require('./app/routes/routes');
 
 // Create bugsnag instance
 // bugsnag.register(process.env.BUGSNAG_API_KEY)
-app.use(bugsnag.requestHandler)
-app.use(bugsnag.errorHandler)
+// app.use(bugsnag.requestHandler);
+// app.use(bugsnag.errorHandler);
 
 // Parse request body to recieve ajax data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.set('views',__dirname + '/views/')
+app.set('views',__dirname + '/views/');
 
 // set the view engine to ejs
-app.engine('ejs', engine)
-app.set('view engine', 'ejs')
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
 
 // public folder to store assets
-app.use(express.static(__dirname + '/public'))
-app.use(express.static(__dirname + '/node_modules'))
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules'));
 
 // routes for app
-app.use('/', routes)
+app.use('/', routes);
 
 // listen on port 3000
-app.listen(3000)
+app.listen(3000);
